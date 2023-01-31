86542b0a53bb31efd72fd7be695f04a9d8218b7e
DimAlus <dima.zvyagin.00@mail.ru>
2023-01-31 13:35:51 +0300

    init

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,226 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+
+using System.Net;
+using System.Net.Sockets;
+using System.IO;
+
+
+
+
+
+namespace Translation
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        BitmapImage img = new BitmapImage();
+
+        /*byte[] bytes = new byte[9294400];
+        int size = 0;
+        bool blockTask = false;
+        bool blockSend = false;
+
+        async void tsk ()
+        {
+            while (true)
+            {
+                await Task.Delay(10);
+                if (size > 0)
+                {
+                    while (blockSend)
+                        await Task.Delay(10);
+                    blockTask = true;
+                    byte[] bts = new byte[size];
+                    Array.Copy(bytes, bts, size);
+                    blockTask = false;
+                    Dispatcher.Invoke(() =>
+                    {
+                        try
+                        {
+
+                            img = new BitmapImage();
+                            img.BeginInit();
+                            img.StreamSource = new MemoryStream(bts);
+                            img.EndInit();
+                            Imag.Source = img;
+                        }
+                        catch (Exception e)
+                        {
+                            //MessageBox.Show(e.ToString());
+                        }
+
+                    }
+                    );
+                }
+            }
+        }*/
+        public MainWindow ()
+        {
+            InitializeComponent();/*
+            Task t = new Task(tsk);
+            t.Start();*/
+        }
+        NetClient client;
+        NetServer server;
+
+        System.Drawing.Rectangle rect = new System.Drawing.Rectangle(new System.Drawing.Point(0, 0),
+                new System.Drawing.Size(System.Windows.Forms.Screen.PrimaryScreen.Bounds.Width,
+                    System.Windows.Forms.Screen.PrimaryScreen.Bounds.Height));
+        private async void Server_Click (object sender, RoutedEventArgs e)
+        {
+            server = new NetServer(CaptureRect);
+            /* IPEndPoint ipEndPoint = new IPEndPoint(IPAddress.Any, 11000);
+
+             Socket myServerSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);*/
+            /*IPEndPoint ipEndPoint;
+            IPHostEntry ipHost = Dns.GetHostEntry("localhost");
+            IPAddress ipAddr = ipHost.AddressList[0];
+            if (isLocal)
+                ipEndPoint = new IPEndPoint(ipAddr, 11000);
+            else
+                ipEndPoint = new IPEndPoint(IPAddress.Any, 11000);
+
+
+            Socket myServerSocket = new Socket(isLocal ? ipAddr.AddressFamily : AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp); ;
+
+
+            try
+            {
+                myServerSocket.Bind(ipEndPoint);
+                myServerSocket.Listen(20);
+                Socket handler = await myServerSocket.AcceptAsync();
+
+                while (true)
+                {
+                    await Task.Delay(1000 / 30);
+                    handler.Send(CaptureRect(rect, System.Drawing.Imaging.ImageFormat.Png));
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }*/
+        }
+
+
+        byte[] CaptureRect ()
+        {
+           // System.Drawing.Rectangle rect;
+            System.Drawing.Imaging.ImageFormat format = System.Drawing.Imaging.ImageFormat.Jpeg;
+            using (var ms = new System.IO.MemoryStream())
+            {
+                using (System.Drawing.Bitmap bitmap = new System.Drawing.Bitmap(rect.Width, rect.Height,
+                    System.Drawing.Imaging.PixelFormat.Format24bppRgb))
+                {
+                    using (System.Drawing.Graphics graphics = System.Drawing.Graphics.FromImage(bitmap))
+                    {
+                        graphics.CopyFromScreen(rect.X, rect.Y, 0, 0, rect.Size, System.Drawing.CopyPixelOperation.SourceCopy);
+                    }
+                    
+                    bitmap.Save(ms, format);
+                }/*
+                var image = new BitmapImage();
+                image.BeginInit();
+                image.CacheOption = BitmapCacheOption.OnLoad;
+                image.StreamSource = ms;
+                image.EndInit();*/
+                return ms.ToArray();
+            }
+        }
+        //bool isLocal = false;
+        private void Client_Click (object sender, RoutedEventArgs e)
+        {
+            IPWindow.Visibility = Visibility.Collapsed;
+            client = new NetClient(ipadd.Text);
+            //SendMessageFromSocket(11000);
+        }
+        //
+        async void SendMessageFromSocket (int port)
+        {
+           /* try
+            {
+
+                // Буфер для входящих данных
+
+
+                // Соединяемся с удаленным устройством
+
+                // Устанавливаем удаленную точку для сокета
+               /* IPEndPoint ipEndPoint;
+                IPHostEntry ipHost = Dns.GetHostEntry("localhost");
+                IPAddress ipAddr = ipHost.AddressList[0];
+                if (isLocal)
+                    ipEndPoint = new IPEndPoint(ipAddr, port);
+                else
+                    ipEndPoint = new IPEndPoint(IPAddress.Parse(/*"192.168.43.215"/*//*ipadd.Text), 11000);
+
+
+                Socket sender = new Socket(isLocal ? ipAddr.AddressFamily : AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp); ;
+
+                // Соединяем сокет с удаленной точкой
+                await sender.ConnectAsync(ipEndPoint);
+
+                while (true)
+                {
+
+                    // Получаем ответ от сервера
+                    while (blockTask)
+                        await Task.Delay(10);
+                    blockSend = true;
+                    size = await sender.ReceiveAsync(new ArraySegment<byte>(bytes, 0, bytes.Length), SocketFlags.None);//(bytes);
+                    blockSend = false;
+
+                    await Task.Delay(100);
+                    /*
+                    byte[] arr = new byte[bytesRec];
+
+                    img = new BitmapImage();
+                    img.BeginInit();
+                    img.StreamSource = new MemoryStream(bytes, 0, bytesRec);
+                    img.EndInit();
+                    Imag.Source = img;
+                    //break;*/
+              /*  }
+
+                // Освобождаем сокет
+                sender.Shutdown(SocketShutdown.Both);
+                sender.Close();
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.Message);
+            }
+            size = 0;*/
+        }
+
+        private void IpCancle_Click (object sender, RoutedEventArgs e)
+        {
+            IPWindow.Visibility = Visibility.Collapsed;
+        }
+        private void IpShow_Click (object sender, RoutedEventArgs e)
+        {
+            IPWindow.Visibility = Visibility.Visible;
+        }
+        /*
+        private void CheckBox_Click (object sender, RoutedEventArgs e)
+        {
+            isLocal = (bool)((CheckBox)sender).IsChecked;
+        }*/
+    }
+}
---
+++ Net.cs
@@ -0,0 +1,204 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Net;
+using System.Net.Sockets;
+using System.IO;
+
+
+
+using System.Windows;
+
+
+namespace Translation
+{
+    delegate void DataEvnt (byte[] data);
+    delegate byte[] GetData ();
+
+    class NetClient
+    {
+        string ipadd = "192.168.43.215";
+        IPEndPoint ipEndPoint;
+        Socket sender;
+        int startPort = 11000;
+        int port = 0;
+
+        public event DataEvnt GetData;
+
+        public NetClient(string idaddress)
+        {
+            ipadd = idaddress;
+            new Task(SendMessageFromSocket).Start();
+        }
+
+        void StartSend ()
+        {
+            IPEndPoint ipEndPoint = new IPEndPoint(IPAddress.Parse(ipadd), startPort);
+            Socket sender = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            sender.Connect(ipEndPoint);
+
+            byte[] bts = new byte[10];
+            sender.Receive(bts);
+            port = bts[0];
+            sender.Shutdown(SocketShutdown.Both);
+            sender.Close();
+        }
+
+        void SendMessageFromSocket ()
+        {
+            if (port == 0)
+                StartSend();
+            try
+            {
+                ipEndPoint = new IPEndPoint(IPAddress.Parse(ipadd), startPort + port);
+                sender = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+                // Соединяем сокет с удаленной точкой
+                sender.Connect(ipEndPoint);
+
+                byte[] bytes = new byte[9000000];
+                while (true)
+                {
+                    sender.Send(new byte[] { 0x1F });
+                    int size = sender.Receive(bytes);
+                    byte[] bts = new byte[size];
+                    Array.Copy(bytes, bts, size);
+                    GetData(bts);
+
+                }
+
+                // Освобождаем сокет
+                sender.Shutdown(SocketShutdown.Both);
+                sender.Close();
+            }
+            catch { }
+            try
+            {
+                sender.Shutdown(SocketShutdown.Both);
+                sender.Close();
+            }
+            catch { }
+
+        }
+
+    }
+    
+    class NetServer
+    {
+        int port = 0;
+        int startPort = 11000;
+        bool EndWork = false;
+
+        public GetData getData;
+
+        List<Socket> MainSockets = new List<Socket>();
+        List<Socket> Sockets = new List<Socket>();
+        bool added = true;
+        byte[] data = null;
+
+        Task dataUpdater;
+
+
+        async void UpdateData ()
+        {
+            while (!EndWork)
+            {
+                data = getData();
+                await Task.Delay(1000 / 15);
+            }
+        }
+
+        public NetServer (GetData GeterData)
+        {
+            getData = GeterData;
+            dataUpdater = new Task(UpdateData);
+            dataUpdater.Start();
+        }
+
+
+        async void ServiceSocket()
+        {
+            int integ = MainSockets.Count;
+            Socket mainSocket = MainSockets[MainSockets.Count - 1];
+            added = true;
+
+            Socket handler = mainSocket.Accept();
+            byte[] rcvbts = new byte[100];
+            while (!EndWork)
+            {
+                try
+                {
+                    handler.Receive(rcvbts);
+                    if (rcvbts[0] == 0x1F)
+                    {
+                        while (data == null)
+                            await Task.Delay(10);
+                        handler.Send(data);
+                    }
+
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.ToString(), "Error of added socket " + integ.ToString());
+                }
+            }
+            handler.Shutdown(SocketShutdown.Both);
+            handler.Close();
+
+            mainSocket.Shutdown(SocketShutdown.Both);
+            mainSocket.Close();
+            MainSockets.Remove(mainSocket);
+        }
+
+        async void CreateSocket(int port)
+        {
+            IPEndPoint endPoint = new IPEndPoint(IPAddress.Any, startPort + port);
+            Socket serverSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+            serverSocket.Bind(endPoint);
+            serverSocket.Listen(20);
+
+            while (!added)
+                await Task.Delay(10);
+            MainSockets.Add(serverSocket);
+            added = false;
+            new Task(ServiceSocket).Start();
+
+        }
+
+        void StartSend ()
+        {
+
+            IPEndPoint ipEndPoint = new IPEndPoint(IPAddress.Any, startPort);
+
+            Socket myServerSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+
+            myServerSocket.Bind(ipEndPoint);
+            myServerSocket.Listen(20);
+
+            while (!EndWork)
+            {
+                try
+                {
+                    Socket handler = myServerSocket.Accept();
+                    handler.Send(new byte[] { (byte)++port });
+
+                    CreateSocket(port);
+                    handler.Shutdown(SocketShutdown.Both);
+                    handler.Close();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.ToString(), "Error of main socket");
+                }
+            }
+
+            myServerSocket.Shutdown(SocketShutdown.Both);
+            myServerSocket.Close();
+        }
+    }
+}
